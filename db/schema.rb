b116a26ba8557e0f9e970da0989b3e19737b6659
edynak-demo <edynakdemo@gmail.com>
2021-07-19 14:39:16 -0400

    implemented associations between models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_160152) do
+ActiveRecord::Schema.define(version: 2021_07_19_170049) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
